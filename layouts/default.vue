61c23e7c1549806fa63a7708215a6d9fb2dab171
Alexis JC <alexisjc.dev@gmail.com>
2021-05-29 13:51:14 +0200

    initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-app dark>
+    <v-app-bar
+      clipped-left
+      fixed
+      app
+    >
+      <v-toolbar-title>Piiin Me!</v-toolbar-title>
+      <v-spacer />
+    </v-app-bar>
+    <v-main>
+      <v-container>
+        <nuxt />
+      </v-container>
+    </v-main>
+    <v-footer
+      fixed
+      app
+    >
+      <span>&copy; {{ new Date().getFullYear() }}</span>
+    </v-footer>
+  </v-app>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,50 @@
+<template>
+  <v-row justify="center" align="center">
+    <v-col cols="12">
+      <h1 class="text-h4">
+        Help to host the open web!
+      </h1>
+    </v-col>
+    <v-col cols="12" md="4">
+      <v-card>
+        <v-card-title>Website A</v-card-title>
+        <v-card-text>
+          A super website about A
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn>Pin</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-col>
+    <v-col cols="12" md="4">
+      <v-card>
+        <v-card-title>Website B</v-card-title>
+        <v-card-text>
+          A super website about B
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn>Pin</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-col>
+    <v-col cols="12" md="4">
+      <v-card>
+        <v-card-title>Website C</v-card-title>
+        <v-card-text>
+          A super website about C
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn>Pin</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+}
+</script>
