3bdc4e43ec63e422e2998054d040d43cd44200e5
Alexis JC <alexisjc.dev@gmail.com>
2021-06-18 23:00:03 +0200

    fix: try to fix row in prod

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-scroll-y-transition tag="v-row" group mode="out-in">
+  <v-scroll-y-transition tag="div" group leave-absolute class="row">
     <v-col cols="12" class="my-10" key="title">
       <h1 class="text-h4 white--text font-weight-bold" style="font-family: 'Poppins', sans-serif!important;">
         Help to host the open web!
@@ -9,33 +9,33 @@
       <v-text-field dark v-model="search" prepend-inner-icon="mdi-file-search" placeholder="Search CID, name, description..." filled></v-text-field>
     </v-col>
     <v-col tag="v-col" cols="12" md="4" v-for="dapp in dappsFiltered" :key="dapp.slug">
-          <v-card>
-            <v-card-title>
-              <span>{{ dapp.title }}</span>
-              <v-spacer></v-spacer>
-              <v-btn :href="'https://ipfs.io/ipfs/' + dapp.cid" target="_blank" icon>
-                <v-icon color="secondary">mdi-open-in-new</v-icon>
-              </v-btn>
-              
-            </v-card-title>
-            <v-card-text>
-              {{ dapp.description }}
-            </v-card-text>
-            <v-card-actions>
-              <v-spacer></v-spacer>
-              <template>
-                <v-btn v-if="!dapp.isPined" @click="pin(dapp)" outlined color="secondary">
-                  <v-icon left>mdi-pin</v-icon>
-                  Pin
-                </v-btn>
-                <v-btn v-else text @click="unpin(dapp)">
-                  <v-icon left>mdi-pin-off</v-icon>
-                  Unpin
-                </v-btn>
-              </template>
-            </v-card-actions>
-          </v-card>
-        </v-col>
+      <v-card>
+        <v-card-title>
+          <span>{{ dapp.title }}</span>
+          <v-spacer></v-spacer>
+          <v-btn :href="'https://ipfs.io/ipfs/' + dapp.cid" target="_blank" icon>
+            <v-icon color="secondary">mdi-open-in-new</v-icon>
+          </v-btn>
+          
+        </v-card-title>
+        <v-card-text>
+          {{ dapp.description }}
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <template>
+            <v-btn v-if="!dapp.isPined" @click="pin(dapp)" outlined color="secondary">
+              <v-icon left>mdi-pin</v-icon>
+              Pin
+            </v-btn>
+            <v-btn v-else text @click="unpin(dapp)">
+              <v-icon left>mdi-pin-off</v-icon>
+              Unpin
+            </v-btn>
+          </template>
+        </v-card-actions>
+      </v-card>
+    </v-col>
   </v-scroll-y-transition>
 </template>
 
